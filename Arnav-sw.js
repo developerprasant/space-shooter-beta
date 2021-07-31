cc416e1edbae2c4003201043f31ab30b5b12b40d
rajnish000anand <rajnish000anand@gmail.com>
2021-07-31 14:02:55 +0530

    .test new Oldsw.js

---
+++ Arnav-sw.js
@@ -0,0 +1,23 @@
+self.skipWaiting();
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('v3').then(function(cache) {
+      //files to precache
+     return cache.addAll([
+      './',
+      './index.html',
+      './CSS/style.css',
+      './JS/main.js'
+     ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
--- sw.js
+++ sw.js
@@ -1,23 +1,46 @@
-self.skipWaiting();
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('v3').then(function(cache) {
-      //files to precache
-     return cache.addAll([
-      './',
-      './index.html',
-      './CSS/style.css',
-      './JS/main.js'
-     ]);
-   })
- );
-});
+const cacheName = 'v1';
 
-self.addEventListener('fetch', function(e) {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
+const cacheAssats = [
+  './index.html',
+  './CSS/style.css',
+  './JS/main.js'
+]
+
+self.addEventListener('install',e=>{
+  e.waitUntil(
+    caches
+      .open(cacheName)
+      .then(cache=>{
+        cache.adAll(cacheAssets)
+      })
+      .then(()=>self.skipWaiting())
+    
+  )
+})
+
+self.addEventListener('activate',e=>{
+  //Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames=>{
+      return Promice.all(
+        cacheNames.map(cache=>{
+          if(cache !== cacheName){
+            //clearing old cache
+            return caches.delete(cache);
+          }
+        })
+      )
     })
-  );
-});
+  )
+})
+
+self.addEventListener('fetch',e=>{
+  console.log('fetching');
+  e.respondWith(fetch(e.request)
+    .catch(c=>caches.match(e.request)))
+})
+
+
+
+
+
