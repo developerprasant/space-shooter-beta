8bbb36ae9c4462e39651a095bbebe07923cd0040
rajnish000anand <rajnish000anand@gmail.com>
2021-07-31 13:34:13 +0530

    .change

---
+++ JS/newOldsw.js
@@ -0,0 +1,43 @@
+const cacheName = 'v1';
+
+self.addEventListener('install',e=>{})
+
+self.addEventListener('activate',e=>{
+  //Remove unwanted caches
+  e.waitUntil(
+    caches.keys().then(cacheNames=>{
+      return Promice.all(
+        cacheNames.map(cache=>{
+          if(cache !== cacheName){
+            //clearing old cache
+            return caches.delete(cache);
+          }
+        })
+      )
+    })
+  )
+})
+
+self.addEventListener('fetch',e=>{
+  console.log('fetching');
+  e.respondWith(
+    fetch(e.request)
+    .then(res=>{
+      //Make copy clone of response
+      const resClone = res.clone();
+      //open cache
+      caches
+        .open(cacheName)
+        .then(cache=>{
+          //add the response to cache
+          cache.put(e.request,resClone);
+        })
+      return res;
+    }).catch(err=>caches.match(e.request).then(res=>res))
+  );
+})
+
+
+
+
+
--- JS/sw.js
+++ JS/sw.js
@@ -1,59 +1,23 @@
-const cacheName = 'v1';
+self.skipWaiting();
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('slcodepreview').then(function(cache) {
+      //files to precache
+     return cache.addAll([
+      './',
+      './index.html',
+      './CSS/style.css',
+      './JS/main.js'
+     ]);
+   })
+ );
+});
 
-// const cacheAssats = [
-//   './index.html',
-//   './CSS/style.css',
-//   './JS/main.js'
-// ]
-
-self.addEventListener('install',e=>{
-  // e.waitUntil(
-  //   caches
-  //     .open(cacheName)
-  //     .then(cache=>{
-  //       cache.adAll(cacheAssets)
-  //     })
-  //     .then(()=>self.skipWaiting())
-    
-  // )
-})
-
-self.addEventListener('activate',e=>{
-  //Remove unwanted caches
-  e.waitUntil(
-    caches.keys().then(cacheNames=>{
-      return Promice.all(
-        cacheNames.map(cache=>{
-          if(cache !== cacheName){
-            //clearing old cache
-            return caches.delete(cache);
-          }
-        })
-      )
-    })
-  )
-})
-
-self.addEventListener('fetch',e=>{
-  console.log('fetching');
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
   e.respondWith(
-    fetch(e.request)
-    .then(res=>{
-      //Make copy clone of response
-      const resClone = res.clone();
-      //open cache
-      caches
-        .open(cacheName)
-        .then(cache=>{
-          //add the response to cache
-          cache.put(e.request,resClone);
-        })
-      return res;
-    }).catch(err=>caches.match(e.request).then(res=>res))
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
-})
-
-
-
-
-
+});
