e839c7b2ca4a5beab80f96fbae4d070d1a0ecd02
rajnish000anand <rajnish000anand@gmail.com>
2021-08-16 21:37:08 +0530

    added simple spaceship move events

--- JS/game.js
+++ JS/game.js
@@ -7,32 +7,23 @@ export default class {
     this.ctx = canv0.getContext('2d',{alpha:true})
     this.ctx.strokeStyle = '#ffffff';
     this.ctx.lineWidth = devicePixelRatio;
-    
+    this.canv.addEventListener('touchmove',(ev)=>this.move(ev));
+    this.canv.addEventListener('mousemove',(ev)=>this.move(ev));
     this.main();
   }
   main(){
     this.drawShip()
-    // requestAnimationFrame(this.main)
+    this.ctx.clearRect(0,0,this.stats.w,this.stats.h);
+    this.ctx.stroke();
+
+    requestAnimationFrame(()=>this.main())
   }
+  
   drawShip(){
     this.ctx.beginPath();
     const p= this.P;
     
-    this.maker([
-      ['M', [p[0]+25*devicePixelRatio,p[1]] ],
-      ['C', [...p,25*devicePixelRatio,0,Math.PI] ],
-      ['C', [...p,10*devicePixelRatio,0,Math.PI] ],
-      ['L', [p[0]-2*devicePixelRatio,p[1]] ],
-      ['L',[p[0]-2*devicePixelRatio,p[1]-35*devicePixelRatio]],
-      ['L',[p[0]-14*devicePixelRatio,p[1]-28*devicePixelRatio]],
-      ['L',[p[0]-14*devicePixelRatio,p[1]]],
-      ['L',[p[0]+14*devicePixelRatio,p[1]]],
-      ['L',[p[0]+14*devicePixelRatio,p[1]-28*devicePixelRatio]], 
-      ['L',[p[0]+2*devicePixelRatio,p[1]-35*devicePixelRatio]],
-      ['L', [p[0]+2*devicePixelRatio,p[1]] ],
-      ['L',[p[0]+25*devicePixelRatio,p[1]]]
-    ])
-    this.ctx.stroke();
+    this.maker([['M', [p[0]+25*devicePixelRatio,p[1]]],['C', [...p,25*devicePixelRatio,0,Math.PI]],['C', [...p,10*devicePixelRatio,0,Math.PI]],['L', [p[0]-2*devicePixelRatio,p[1]]],['L',[p[0]-2*devicePixelRatio,p[1]-35*devicePixelRatio]],['L',[p[0]-14*devicePixelRatio,p[1]-28*devicePixelRatio]],['L',[p[0]-14*devicePixelRatio,p[1]]],['L',[p[0]+14*devicePixelRatio,p[1]]],['L',[p[0]+14*devicePixelRatio,p[1]-28*devicePixelRatio]], ['L',[p[0]+2*devicePixelRatio,p[1]-35*devicePixelRatio]],['L', [p[0]+2*devicePixelRatio,p[1]]],['L',[p[0]+25*devicePixelRatio,p[1]]]])
   }
   // rander(){}
   maker(r){
@@ -44,4 +35,11 @@ export default class {
     }
     for(let i of r)a[i[0]](i[1]);
   }
+  
+  move(ev){
+    let x = (ev.clientX||ev.touches[0].clientX) *devicePixelRatio;
+    let y = (ev.clientY||ev.touches[0].clientY) *devicePixelRatio;
+    this.P=[x,y];
+  }
+  
 }
